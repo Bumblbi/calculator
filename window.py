2bac5b847731e15f303736eac991361e20c14580
Bumblby <svahrusev45@yandex.ru>
2023-10-08 09:18:15 +0500

    Добавил кнопки на калькулятор

--- window.py
+++ window.py
@@ -8,6 +8,19 @@ class MainApp(App):
         main_layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
         self.solution = TextInput(multiline=False, readonly=False, halign="right", font_size=55, input_filter="float")
         main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"]
+        ]
+
+        for row in buttons:
+            horizontal_layout = BoxLayout()
+            for lable in row:
+                button = Button(text=lable, pos_hint={"center_x": 0.5, "center_y": 0.5})
+                horizontal_layout.add_widget(button)
+            main_layout.add_widget(horizontal_layout)
 
         return main_layout
 
